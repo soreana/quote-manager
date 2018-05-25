dea304102dc28e89e2fbb1dcce769ac0e72cd263
Sina Kashi Pazha <esterlinkof@gmail.com>
2018-05-25 17:45:12 +0430

    Create Verse Entity to save in database.

--- app/ir/kashipazha/quotemanager/poem/Verse.java
+++ app/ir/kashipazha/quotemanager/poem/Verse.java
@@ -1,4 +1,28 @@
 package ir.kashipazha.quotemanager.poem;
 
+import ir.kashipazha.commons.HibernateUtil;
+import org.hibernate.Session;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "verse")
 public class Verse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "verse_pk", nullable = false, unique = true, updatable = false)
+    private long id;
+
+    @Column(name = "first", nullable = false)
+    private String first;
+
+    @Column(name = "last", nullable = false)
+    private String last;
+
+    Verse(String first, String last){
+        this.first = first;
+        this.last = last;
+    }
+
+    public Verse(){}
 }
